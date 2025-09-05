fb7f43dafc03176ea698aea6fc8854469cda8468
Erin DeLong <erindelong@Erins-MacBook-Pro.local>
2025-09-04 22:49:03 -0700

    returning status code

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import os
+from dotenv import load_dotenv
+import requests
+from fastapi import Request
 
 
 app = FastAPI(title="Trinity API", version="0.1.0")
@@ -8,6 +11,8 @@ app = FastAPI(title="Trinity API", version="0.1.0")
 # Configure CORS. Optionally set CORS_ORIGINS to a comma-separated list in env.
 _cors_env = os.environ.get("CORS_ORIGINS", "*")
 allow_origins = ["*"] if _cors_env.strip() == "*" else [o.strip() for o in _cors_env.split(",") if o.strip()]
+
+load_dotenv()
 
 app.add_middleware(
     CORSMiddleware,
@@ -24,8 +29,27 @@ def read_root():
 
 
 @app.get("/health")
-def health():
-    return {"status": "ok"}
+def health(request: Request):
+    url = os.getenv("URL")
+    params = {
+        "shipmentId": request.query_params.get("shipmentId"),
+        "mcNumber": request.query_params.get("mcNumber"),
+        "dotNumber": request.query_params.get("dotNumber"),
+        "contactName": request.query_params.get("contactName"),
+        "contactPhoneNumber": request.query_params.get("contactPhoneNumber"),
+        "currentCity": request.query_params.get("currentCity"),
+        "currentState": request.query_params.get("currentState"),
+        "note": request.query_params.get("note"),
+        "offeredRate": request.query_params.get("offeredRate"),
+        "callRate": request.query_params.get("callRate")
+    }
+    try:
+        response = requests.post(url, json=params, headers={"Content-Type": "application/json"})
+        print(response.status_code)
+        return {"status": response.status_code}
+    except Exception as e:
+        print(e)
+        return {"status": response.status_code}
 
 
 @app.get("/env")
